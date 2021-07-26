b38c039b90199a75835e741f3b46840d353127c1
Joshua Salzedo <joshuasalzedo@gmail.com>
2021-07-26 13:59:45 -0700

    Flesh out tim8 handler

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@
 #![allow(unused_imports)]
 
 use panic_rtt_target as _panic_handler;
-
+mod tim8;
 #[rtic::app(device = stm32f4xx_hal::stm32, peripherals = true)]
 mod app {
-
+    use crate::tim8::tim8_cc;
     use stm32f4xx_hal::prelude::*;
     use stm32f4xx_hal::timer::Timer;
     use stm32f4xx_hal::pwm_input::PwmInput;
@@ -15,10 +15,11 @@ mod app {
     use stm32f4xx_hal::gpio::gpioc::PC6;
     use stm32f4xx_hal::gpio::Alternate;
 
-    type PwmMonitor = PwmInput<TIM8, PC6<Alternate<3>>>;
-
+    pub(crate) type PwmMonitor = PwmInput<TIM8, PC6<Alternate<3>>>;
     #[shared]
     struct Shared {
+        /// the last observed position of the turret
+        last_observed_turret_position: f32
     }
 
     #[local]
@@ -32,6 +33,7 @@ mod app {
         let rcc = ctx.device.RCC.constrain();
         // then retreive the clocks, so we can configure timers later on
         let clocks = rcc.cfgr.freeze();
+
 
         // obtain a reference to the GPIOC register block, so we can configure pins on the PC bus.
         let gpioc = ctx.device.GPIOC.split();
@@ -48,9 +50,16 @@ mod app {
 
         // lastly return the shared and local resources, as per RTIC's spec.
             (Shared {
-
+                last_observed_turret_position: 0.0,
             }, Local {
                 monitor
             }, init::Monotonics())
+    }
+    // RTIC docs specify we can modularize the code by using these `extern` blocks.
+    // This allows us to specify the handlers in other modules and still work as RTIC interrupt
+    // handlers.
+    extern "Rust" {
+        #[task(binds=TIM8_CC, local=[monitor], shared=[last_observed_turret_position])]
+        fn tim8_cc(context: tim8_cc::Context);
     }
 }
