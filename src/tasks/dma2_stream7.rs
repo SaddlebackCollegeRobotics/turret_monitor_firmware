6c73ebfc3f54445694cc5e42eb154940cc805794
Joshua Salzedo <jsalzedo0@saddleback.edu>
2021-07-30 12:08:05 -0700

    restructure to minimize code in the unsafe.

--- src/tasks/dma2_stream7.rs
+++ src/tasks/dma2_stream7.rs
@@ -5,12 +5,18 @@ use rtt_target::rprintln;
 pub(crate) fn on_dma2_stream7(ctx: on_dma2_stream7::Context){
     let dma_state: TxBufferState = ctx.shared.send.take().expect("failed to aquire buffer state");
     match dma_state{
-        TxBufferState::Running(tx) => {
-            *ctx.shared.send = Some(TxBufferState::Idle(tx))
+        TxBufferState::Running(mut tx) => {
+            // turns out DMA doesn't clean up its own interrupts, so we have to do so ourselves.
+            tx.clear_transfer_complete_interrupt();
+            // nothing left to transfer, pause DMA.
+            tx.pause(|_| {});
+            *ctx.shared.send = Some(TxBufferState::Idle(tx));
+
         }
         TxBufferState::Idle(mut tx) => {
+            // this shouldn't happen.
             rprintln!("DMA shouldn't be firing interrupts while we are idle.");
-            tx.pause(|f| {});
+            tx.pause(|_| {});
             *ctx.shared.send = Some(TxBufferState::Idle(tx))
         }
     }
--- src/tasks/periodic_update.rs
+++ src/tasks/periodic_update.rs
@@ -5,7 +5,7 @@ use crate::app::{Usart1DMATransferTx, Usart1Buf, Usart1, BUF_SIZE};
 use rtic::mutex_prelude::*;
 use stm32f4xx_hal::prelude::*;
 use embedded_dma::WriteTarget;
-
+const PERODIC_DELAY: Seconds = Seconds(1u32);
 pub enum TxBufferState {
     // Ready, use the contained buffer for next transfer
     Running(Usart1DMATransferTx),
@@ -21,33 +21,42 @@ pub(crate) fn periodic_emit_status(
 ) {
     rprintln!("tick!");
 
-    let mut turret_position: f32 = 0.0;
     /*
         entering critical section
     */
-    context.shared.last_observed_turret_position.lock(|guard| {
-        turret_position = *guard;
-    });
+    let turret_position:f32 = context.shared.last_observed_turret_position.lock(|guard| {*guard });
     /*
         leaving critical section
     */
-
+    // retrieve the DMA state
     let dma_state: TxBufferState = context.shared.send.take().expect("failed to aquire buffer state");
+
+    let mut payload: [u8; BUF_SIZE] = [0x00;BUF_SIZE];
+    payload[0..4].copy_from_slice(&turret_position.to_be_bytes());
+    payload[5] = ',' as u8;
+
+    // if the DMA is idle, start a new transfer.
     if let TxBufferState::Idle(mut tx) = dma_state {
         rprintln!("DMA was idle, setting up next transfer...");
-
+        // SAFETY: memory corruption can occur in double-buffer mode in the event of an overrun.
+        //   - we are in single-buffer mode so this is safe.
         unsafe {
+            // We re-use the existing DMA buffer, since the buffer has to live for 'static
+            // in order to be safe. This was ensured during creation of the Transfer object.
             tx.next_transfer_with(|buf, _| {
-                // buf[0..4].clone_from_slice(&turret_position.to_be_bytes());
-                // buf[5] = ',' as u8;
-                buf.fill(0x00);
-                buf[0..4].copy_from_slice(&[0xDE, 0xAD, 0xBE, 0xEF]);
+                // populate the DMA buffer with the new bufer's content
+                buf.copy_from_slice(&payload);
+                // log the TX buffer
                 rprintln!("buf :: {:?}", buf);
+                // calculate the buffer's length, if only to satisfy the closure's contract.
                 let buf_len= buf.len();
-                    (buf, 4)
+                    (buf, buf_len) // Don't know what the second argument is, but it seems to be ignored.
             }) .expect("Something went horribly wrong setting up the transfer.");
         }
+        // update the DMA state into the running phase
         *context.shared.send = Some(TxBufferState::Running(tx));
+    } else {
+        rprintln!("[WARNING] periodic ticked but a previous DMA was still active!");
     };
 
     // serial.write_fmt(format_args!("{}", turret_position)).expect("failed to write to UARt");
@@ -57,6 +66,6 @@ pub(crate) fn periodic_emit_status(
 
 fn reschedule_periodic() {
     // re-schedule this task (become periodic)
-    crate::app::periodic_emit_status::spawn_after(Seconds(1u32))
+    crate::app::periodic_emit_status::spawn_after(PERODIC_DELAY)
         .expect("failed to re-spawn periodic task.");
 }
